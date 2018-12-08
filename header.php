4a7da7639a4eff44571633e2ae11a55d3089e49b
Axel M. (amacz13) <axel.maczkowiak@live.fr>
2018-12-08 11:18:32 +0100

    Centered Header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   
   <nav class="cyan darken-2">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">SoundBox</a>
+      <a href="#" class="brand-logo center">SoundBox</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="soundboard.html">SoundBoard</a></li>
         <li><a href="login.php">Connexion</a></li>
