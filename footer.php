5044b278f6e05292a40fd784d5d4bb35f978a953
Unknown <albane.gril@free.fr>
2018-12-08 10:54:09 +0100

    include header + footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="page-footer cyan darken-2">
+  <div class="container">
+    <div class="row">
+      <div class="col l12 s12">
+        <h5 class="white-text">Projet SoundBoard</h5>
+        <p class="grey-text text-lighten-4">Dans le cadre du Hackaton Jensim</p>
+      </div>
+    </div>
+  </div>
+</footer>
--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <header>
-        
-        <nav>
-          <div class="nav-wrapper">
-            <a href="#" class="brand-logo">SoundBox</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="soundboard.html">SoundBoard</a></li>
-              <li><a href="#">Compte</a></li>
-            </ul>
-          </div>
-        </nav>
-        
-      </header>
+  
+  <nav class="cyan darken-2">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">SoundBox</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="soundboard.html">SoundBoard</a></li>
+        <li><a href="login.php">Connexion</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+</header>
