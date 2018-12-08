55bd180b3f3c42c1e8d85401413ff7f925b91bdd
Unknown <albane.gril@free.fr>
2018-12-08 15:12:50 +0100

    include header

--- soundboard.php
+++ soundboard.php
@@ -13,26 +13,7 @@
     </head>
 
     <body class="cyan lighten-5">
-      <header>
-  
-        <nav class="cyan darken-2">
-          <div class="nav-wrapper">
-            <a href="soundboard.php" class="brand-logo center">SoundBox</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <a id="menu" class="waves-effect waves-light btn btn-floating" ><i class="material-icons left">settings</i></a>
-            <div class="tap-target" data-target="menu">
-            <div class="tap-target-content">
-              <p>Mode édition activé</p>
-            </div>
-          </div>
-              <li><a href="soundboard.php">SoundBoard</a></li>
-              <li><a href="compte.php">Compte</a></li>
-              <li><a href="login.php">Connexion/Inscription</a></li>
-            </ul>
-          </div>
-        </nav>
-  
-      </header>
+      <?php include_once ("header.php"); ?>
 
       <div class="container">
         <div class="row">
