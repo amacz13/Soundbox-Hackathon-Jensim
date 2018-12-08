68219d9c935dd9f95e96bdd38e11d516d81cd9ad
AfuaDOM <sandradomyes@gmail.com>
2018-12-08 15:48:37 +0100

    boutonadd

--- soundboard.php
+++ soundboard.php
@@ -65,6 +65,40 @@
 
       </div>
 
+      <!-- Modal Structure -->
+      <div id="modal1" class="modal">
+        <div class="modal-content">
+        <form action="php/login.fn.php" method="post">
+                <div class="card-action">
+                    <div class="row">
+                        <div class="input-field col s12">
+                        <input id="login" type="text" class="validate" name="texte">
+                        <label for="login">Nom du bouton</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="url" type="url" class="validate" name="url">
+                            <label for="url">Url du bouton</label>
+                        </div>
+                    </div>
+                    <div class="row valign-wrapper">
+                        <div class="col s6">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">Valider</button>
+                        </div>
+                        <div class="col s6">
+                            <a href="register.php" class="right">Inscription</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat">Confirmer</a>
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat">Annuler</a>
+        </div>
+      </div>
+
       <?php include_once ("footer.php"); ?>
       
       <!--JavaScript at end of body for optimized loading-->
