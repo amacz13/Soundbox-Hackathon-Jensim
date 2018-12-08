db8c0a1c38b2a2bae2bd193e7c6cbc3db7375f26
Axel M. (amacz13) <axel.maczkowiak@live.fr>
2018-12-08 11:19:11 +0100

    Fixing nav links

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
   
   <nav class="cyan darken-2">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">SoundBox</a>
+      <a href="soundboard.php" class="brand-logo center">SoundBox</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="soundboard.html">SoundBoard</a></li>
+        <li><a href="soundboard.php">SoundBoard</a></li>
         <li><a href="login.php">Connexion</a></li>
       </ul>
     </div>
